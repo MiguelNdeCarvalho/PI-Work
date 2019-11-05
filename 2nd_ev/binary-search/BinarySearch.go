01ccd4069f06038f2479d8ca1caa0ffb4e014133
RuiCachapa <l37617@alunos.uevora.pt>
2019-11-05 01:26:12 +0000

    BinarySearch:DONE AND FIXED

--- 2nd_ev/binary-search/BinarySearch.go
+++ 2nd_ev/binary-search/BinarySearch.go
@@ -1,30 +1,3 @@
-/*package main
-import "fmt"
-
-func pesquisaBinaria(slice int, key []int) int {
-
-    low := 0
-    high := len(key) - 1
-
-    for low <= high{
-        median := (low + high) / 2
-
-        if key[median] < slice {
-            low = median + 1
-        }
-        if key[median] == slice {
-            return key[median]
-
-        }else{
-        high = median - 1
-        }
-    }
-    return -1
-
-func main(){
-    fmt.Printf("%d/n",pesquisaBinaria(63,{1,2, 9, 20, 31, 45, 63, 70, 100} ))
-}
-*/
 package main
 import (
     "fmt"
