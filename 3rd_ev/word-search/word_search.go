7a415c626084009d14e93b8920a457f4b3c6a61e
RuiCachapa <l37617@alunos.uevora.pt>
2019-11-26 20:24:48 +0000

    Progress:word-search

--- 3rd_ev/word-search/word_search.go
+++ 3rd_ev/word-search/word_search.go
@@ -1,1 +1,34 @@
 package wordsearch
+
+import (
+	"errors"
+)
+
+var puzzleWords [][]byte
+
+var (
+	ErrNoWordInPuzzle = errors.New("no word in puzzle")
+)
+
+type Direction [2]int // [2]int{row_offset, col_offset}
+
+var Directions = []Direction{
+	{-1, 0},  // North
+	{-1, 1},  // NorthEast
+	{0, 1},   // East
+	{1, 1},   // SouthEast
+	{1, 0},   // South
+	{1, -1},  // SouthWest
+	{0, -1},  // West
+	{-1, -1}, // NorthWest
+}
+
+type Result struct {
+	endPos [2]int
+	word   string
+}
+
+type TrieNode struct {
+	word string
+	next [26]*TrieNode
+}
