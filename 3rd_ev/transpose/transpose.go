ab189ac8680f96ccb911ab475ef08685715598e0
MarcoFig <l37724@alunos.uevora.pt>
2019-11-27 19:13:54 +0000

    Transpose: Done

--- 3rd_ev/transpose/transpose.go
+++ 3rd_ev/transpose/transpose.go
@@ -1,1 +1,26 @@
 package transpose
+
+func Transpose(input []string) []string {
+
+	output := []string{}
+
+	for linha1, linha2 := range input {
+
+		for coluna, i := range linha2 {
+
+			if len(output) <= coluna {
+
+				output = append(output, "")
+			}
+
+			for len(output[coluna]) < linha1 {
+
+				output[coluna] += " "
+			}
+
+			output[coluna] += string(i)
+		}
+	}
+
+	return output
+}
